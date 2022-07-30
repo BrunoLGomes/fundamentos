3eadc42c23b0b6d7b47b72ad11a5af1b1513b28d
Bruno Gomes <brunoDEVJ@gmail.comgit config --global user.email brunoDEVJ@gmail.com>
2022-07-29 22:00:02 -0300

    Finalização dos fundamentos

--- src/components/Navegador.jsx
+++ src/components/Navegador.jsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
-import Style from "../styles/Navegador.module.css"
+import Style from "../styles/Navegador.module.css";
 
-
-export default function Navegador(props){
-    return (
-        <Link href={props.destino}>
-            <div className={Style.navegador} style={{
-                backgroundColor: props.cor ?? 'dodgerblue'}}>
-            {props.texto}
-            </div>
-        </Link>
-    )
+export default function Navegador(props) {
+  return (
+    <Link href={props.destino} passHref>
+      <div
+        className={Style.navegador}
+        style={{
+          backgroundColor: props.cor ?? "dodgerblue",
+        }}
+      >
+        {props.texto}
+      </div>
+    </Link>
+  );
 }
--- src/pages/api/Clientes/index.js
+++ src/pages/api/Clientes/index.js
@@ -1,3 +1,16 @@
-export default function cliente(req, res) {
-    res.status(200).send()
+export default function handler(req, res) {
+    
+    if(req.method === "GET") {
+        handleget(req, res)
+    } else {
+        res.status(405).send()
+    }
+}
+
+function handleget(req, res){
+    res.status(200).json({
+        id: 3,
+        nome: 'blabla',
+        email: 'blablabla@gmail.com.br'
+    })
 }
